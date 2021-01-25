3e895713994b526e49af33c32a866294910a9478
Eirik Tobiassen <eiriktob@stud.ntnu.no>
2021-01-25 22:18:59 +0100

    Own printing of fps

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
+use bevy::diagnostic::Diagnostics;
 use bevy::{
-    diagnostic::{FrameTimeDiagnosticsPlugin, PrintDiagnosticsPlugin},
+    diagnostic::{DiagnosticId, FrameTimeDiagnosticsPlugin},
     prelude::*,
     window::WindowMode,
 };
-
 struct Game;
 
+const FPS_ID: DiagnosticId = DiagnosticId::from_u128(288146834822086093791974408528866909483);
+
 impl Plugin for Game {
-    fn build(&self, app: &mut AppBuilder) {}
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_system(print_fps.system());
+    }
 }
 
 fn main() {
@@ -25,7 +29,12 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_plugin(FrameTimeDiagnosticsPlugin)
-        .add_plugin(PrintDiagnosticsPlugin::default())
         .add_plugin(Game)
         .run();
+}
+
+fn print_fps(diagnostics: Res<Diagnostics>) {
+    if let Some(value) = diagnostics.get(FPS_ID).unwrap().value() {
+        println!("{}", value);
+    }
 }
