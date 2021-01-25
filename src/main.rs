1d943c5f7ec8d47cc23e5a8f19583e406687faf1
Eirik Tobiassen <eiriktob@stud.ntnu.no>
2021-01-26 00:30:09 +0100

    Initial window with fps counter

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,16 @@
+//#![windows_subsystem = "windows"]
+
 use bevy::diagnostic::Diagnostics;
-use bevy::{
-    diagnostic::{DiagnosticId, FrameTimeDiagnosticsPlugin},
-    prelude::*,
-    window::WindowMode,
-};
-struct Game;
-
-const FPS_ID: DiagnosticId = DiagnosticId::from_u128(288146834822086093791974408528866909483);
-
-impl Plugin for Game {
-    fn build(&self, app: &mut AppBuilder) {
-        app.add_system(print_fps.system());
-    }
-}
+use bevy::{diagnostic::FrameTimeDiagnosticsPlugin, prelude::*, window::WindowMode};
 
 fn main() {
     App::build()
         .add_resource(WindowDescriptor {
             title: "Game".to_string(),
-            width: 1200.0,
-            height: 900.0,
+            width: 800.0,
+            height: 600.0,
             vsync: false,
-            resizable: false,
+            resizable: true,
             mode: WindowMode::Windowed,
             cursor_locked: false,
             cursor_visible: true,
@@ -29,12 +18,47 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_plugin(FrameTimeDiagnosticsPlugin)
-        .add_plugin(Game)
+        .add_startup_system(setup.system())
+        .add_system(update_fps.system())
         .run();
 }
 
-fn print_fps(diagnostics: Res<Diagnostics>) {
-    if let Some(value) = diagnostics.get(FPS_ID).unwrap().value() {
-        println!("{}", value);
+fn setup(commands: &mut Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(CameraUiBundle::default()).spawn(TextBundle {
+        text: Text {
+            value: "FPS: ".to_string(),
+            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+            style: TextStyle {
+                font_size: 25.0,
+                alignment: TextAlignment {
+                    vertical: VerticalAlign::Top,
+                    horizontal: HorizontalAlign::Left,
+                },
+                color: Color::AQUAMARINE,
+            },
+        },
+        style: Style {
+            position_type: PositionType::Absolute,
+            position: Rect {
+                top: Val::Px(5.0),
+                left: Val::Px(10.0),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+}
+
+fn update_fps(diagnostics: Res<Diagnostics>, mut query: Query<&mut Text>) {
+    if let Some(value) = diagnostics
+        .get(FrameTimeDiagnosticsPlugin::FPS)
+        .unwrap()
+        .value()
+    {
+        let fps = value as i32;
+        for mut text in query.iter_mut() {
+            text.value = "FPS: ".to_owned() + &fps.to_string();
+        }
     }
 }
