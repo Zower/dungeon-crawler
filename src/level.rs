1da0a88647c38a67928f1842a8cd0ce0526f7012
Eirik Tobiassen <eiriktob@stud.ntnu.no>
2021-01-29 01:24:04 +0100

    Movement equal on all fps, only 4-directional

--- src/level.rs
+++ src/level.rs
@@ -3,13 +3,27 @@
 use bevy::prelude::*;
 
 const _TILE_SIZE: f32 = 32.0;
+
 /// A level to be played on
+#[derive(Debug)]
 pub struct Level {
+    /// A list of level tiles
     grid: Vec<GridPiece>,
 }
+#[derive(Debug, Copy, Clone)]
+pub enum TileType {
+    Floor,
+    Wall,
+}
 
-/// A builder for levels
+#[derive(Debug)]
+pub struct TileTexture {
+    texture: Handle<ColorMaterial>,
+    tile: TileType,
+}
+
 pub struct LevelBuilder {
+    textures: Vec<TileTexture>,
     size: LevelSize,
 }
 
@@ -17,6 +31,7 @@ impl LevelBuilder {
     pub fn new(length: i32, height: i32) -> Self {
         LevelBuilder {
             size: LevelSize { length, height },
+            ..Default::default()
         }
     }
 
@@ -26,19 +41,44 @@ impl LevelBuilder {
                 length: size,
                 height: size,
             },
+            ..Default::default()
         }
     }
 
-    pub fn build(&self) -> Level {
+    pub fn add_texture(
+        &mut self,
+        tile_type: TileType,
+        texture: Handle<ColorMaterial>,
+    ) -> &mut Self {
+        self.textures.push(TileTexture {
+            texture: texture,
+            tile: tile_type,
+        });
+        self
+    }
+
+    pub fn build(&self) -> Result<Level, EmptyTextureError> {
         let mut grid = Vec::new();
+
+        if self.textures.len() == 0 {
+            println!("Trying to build with no textures");
+            return Err(EmptyTextureError); //Custom error type yada yada
+        }
 
         for x in 0..self.size.length {
             for y in 0..self.size.height {
-                grid.push(GridPiece { x, y })
+                grid.push(GridPiece {
+                    texture: TileTexture {
+                        texture: self.textures[0].texture.clone(),
+                        tile: self.textures[0].tile.clone(),
+                    }, // This is temporarily only the first texture
+                    x,
+                    y,
+                })
             }
         }
 
-        Level { grid }
+        Ok(Level { grid })
     }
 
     pub fn print(&self) {
@@ -50,6 +90,7 @@ impl Default for LevelBuilder {
     fn default() -> Self {
         LevelBuilder {
             size: LevelSize::default(),
+            textures: Vec::<TileTexture>::new(),
         }
     }
 }
@@ -74,11 +115,16 @@ impl Default for LevelSize {
 ///One piece on the grid, will have a texture
 #[derive(Debug)]
 struct GridPiece {
+    texture: TileTexture,
     x: i32,
     y: i32,
 }
 
 impl Level {
+    pub fn get_texture(&self, grid_pos: i32) -> Handle<ColorMaterial> {
+        self.grid[0].texture.texture.clone()
+    }
+
     pub fn print(&self) {
         println!("{:?}", self.grid)
     }
@@ -97,11 +143,11 @@ impl Level {
 // fn setup(
 //     commands: &mut Commands,
 //     mut materials: ResMut<Assets<ColorMaterial>>,
-//     //asset_server: Res<AssetServer>,
+//     asset_server: Res<AssetServer>,
 //     size: Res<LevelSize>,
 //     //windows: Res<Windows>,
 // ) {
-//     //let texture_tile = asset_server.load("tiles/floor.png");
+//     let texture_tile = asset_server.load("tiles/floor.png");
 
 //     //let window = windows.get_primary().unwrap();
 
@@ -113,8 +159,8 @@ impl Level {
 //         for _ in 0..size.height {
 //             level_batch_sprite.push(SpriteBundle {
 //                 sprite: Sprite::new(Vec2::new(TILE_SIZE, TILE_SIZE)),
-//                 //material: materials.add(texture_tile.clone().into()),
-//                 material: materials.add(Color::rgb(0.5, 0.5, 1.0).into()),
+//                 material: materials.add(texture_tile.clone().into()),
+//                 //material: materials.add(Color::rgb(0.5, 0.5, 1.0).into()),
 //                 transform: Transform {
 //                     translation: Vec3::new(posx, posy, 0.0),
 //                     ..Default::default()
@@ -129,3 +175,11 @@ impl Level {
 
 //     commands.spawn_batch(level_batch_sprite);
 // }
+
+pub struct EmptyTextureError;
+
+impl std::fmt::Debug for EmptyTextureError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Tried to build with no textures set!")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,30 @@ use fps_diagnostic::FPSScreenDiagnostic;
 
 use level::LevelBuilder;
 
+use bevy::input::keyboard::KeyboardInput;
 use bevy::prelude::*;
 
 struct Player;
+
+struct MoveState(Direction);
+
+struct MoveTimer(Timer);
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+    Still,
+}
+
 #[derive(Debug)]
 struct GridPosition {
     x: i32,
     y: i32,
 }
+
+struct Levels(Vec<level::Level>);
 
 // struct Size {
 //     length: i32,
@@ -36,20 +52,100 @@ fn main() {
         })
         .add_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
         .add_resource(LevelBuilder::default())
+        .add_resource(Levels(Vec::<level::Level>::new()))
+        .add_resource(MoveState(Direction::Still))
+        .add_resource(MoveTimer(Timer::from_seconds(0.016, true)))
         .add_plugins(DefaultPlugins)
-        //.add_plugin(LevelPlugin)
         .add_plugin(FPSScreenDiagnostic)
-        .add_startup_system(setup.system())
-        .add_system(move_player_grid.system())
-        .add_system(move_player_transform.system())
-        .add_system(update_camera.system())
         .add_startup_system(test_builder.system())
+        .add_startup_system(setup.system())
+        .add_system(update_direction.system())
+        .add_system(move_player.system())
+        .add_system(update_camera.system())
         .run();
 }
 
-fn test_builder(level_builder: Res<LevelBuilder>) {
-    let level = level_builder.build();
-    level.print();
+fn test_builder(
+    commands: &mut Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+    mut level_builder: ResMut<LevelBuilder>,
+) {
+    let level = level_builder
+        .add_texture(
+            level::TileType::Floor,
+            materials.add(asset_server.load("tiles/floor.png").into()),
+        )
+        .build()
+        .unwrap();
+
+    //level.print();
+
+    commands.spawn(SpriteBundle {
+        material: level.get_texture(0),
+        ..Default::default()
+    });
+}
+
+fn update_camera(
+    mut query_cam: Query<(&bevy::render::camera::Camera, &mut Transform)>,
+    query_player: Query<&Transform, With<Player>>,
+) {
+    for (cam, mut trans_cam) in query_cam.iter_mut() {
+        if cam.name == Some(String::from("Camera2d")) {
+            for trans_player in query_player.iter() {
+                trans_cam.translation.x = trans_player.translation.x;
+                trans_cam.translation.y = trans_player.translation.y;
+            }
+        }
+    }
+}
+
+fn update_direction(mut move_state: ResMut<MoveState>, keyboard_input: Res<Input<KeyCode>>) {
+    if keyboard_input.pressed(KeyCode::W) {
+        move_state.0 = Direction::Up;
+    }
+    if keyboard_input.pressed(KeyCode::A) {
+        move_state.0 = Direction::Left;
+    }
+    if keyboard_input.pressed(KeyCode::S) {
+        move_state.0 = Direction::Down;
+    }
+    if keyboard_input.pressed(KeyCode::D) {
+        move_state.0 = Direction::Right;
+    }
+    if keyboard_input.just_released(KeyCode::W)
+        || keyboard_input.just_released(KeyCode::A)
+        || keyboard_input.just_released(KeyCode::S)
+        || keyboard_input.just_released(KeyCode::D)
+    {
+        move_state.0 = Direction::Still
+    }
+}
+
+fn move_player(
+    time: Res<Time>,
+    mut timer: ResMut<MoveTimer>,
+    move_state: Res<MoveState>,
+    mut query: Query<(&mut GridPosition, &mut Transform), With<Player>>,
+) {
+    if !timer.0.tick(time.delta_seconds()).just_finished() {
+        return;
+    }
+
+    for (mut pos, mut transform) in query.iter_mut() {
+        match move_state.0 {
+            Direction::Up => pos.y += 1,
+            Direction::Down => pos.y -= 1,
+            Direction::Left => pos.x -= 1,
+            Direction::Right => pos.x += 1,
+            Direction::Still => (),
+        }
+        transform.translation.y = pos.y as f32 * 5.0;
+        transform.translation.x = pos.x as f32 * 5.0;
+        transform.translation.y = pos.y as f32 * 5.0;
+        transform.translation.x = pos.x as f32 * 5.0;
+    }
 }
 
 fn setup(
@@ -95,47 +191,4 @@ fn setup(
         })
         .with(Player)
         .with(GridPosition { x: 0, y: 0 });
-}
-
-fn update_camera(
-    mut query_cam: Query<(&bevy::render::camera::Camera, &mut Transform)>,
-    query_player: Query<&Transform, With<Player>>,
-) {
-    for (cam, mut trans_cam) in query_cam.iter_mut() {
-        if cam.name == Some(String::from("Camera2d")) {
-            for trans_player in query_player.iter() {
-                trans_cam.translation.x = trans_player.translation.x;
-                trans_cam.translation.y = trans_player.translation.y;
-            }
-        }
-    }
-}
-
-fn move_player_grid(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut grid: Query<&mut GridPosition, With<Player>>,
-) {
-    for mut pos in grid.iter_mut() {
-        if keyboard_input.pressed(KeyCode::W) {
-            pos.y += 1;
-        }
-        if keyboard_input.pressed(KeyCode::A) {
-            pos.x -= 1;
-        }
-        if keyboard_input.pressed(KeyCode::S) {
-            pos.y -= 1;
-        }
-        if keyboard_input.pressed(KeyCode::D) {
-            pos.x += 1;
-        }
-    }
-}
-
-fn move_player_transform(mut query: Query<(&GridPosition, &mut Transform), With<Player>>) {
-    for (pos, mut transform) in query.iter_mut() {
-        transform.translation.y = pos.y as f32 * 5.0;
-        transform.translation.x = pos.x as f32 * 5.0;
-        transform.translation.y = pos.y as f32 * 5.0;
-        transform.translation.x = pos.x as f32 * 5.0;
-    }
 }
